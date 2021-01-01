092f6076aa7065a837f15d73d9217d4e47e9856c
oshinkoshinko <aiueokakatyuka.9@gmail.com>
2021-01-01 12:04:34 +0000

    make comment table

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
 
+  get 'create/destroy'
   devise_for :users
   resources :users,only: [:show,:index,:edit,:update]
-  resources :books
+  resources :books do
+  resources :book_comments,only: [:create, :destroy]
+  end
   root 'homes#top'
   get 'home/about' => 'homes#about'
 end
