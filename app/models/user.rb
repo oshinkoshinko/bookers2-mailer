ad52adcdb25f382d295f56e04a969ec56ecd062c
oshinkoshinko <aiueokakatyuka.9@gmail.com>
2021-01-05 14:33:50 +0000

    [Finish] test7

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,6 +30,17 @@ class User < ApplicationRecord
    following.include?(other_user)
   end
 
+  include JpPrefecture
+  jp_prefecture :prefecture_code
+
+  def prefecture_name
+   JpPrefecture::Prefecture.find(code: prefecture_code).try(:name)
+  end
+
+  def prefecture_name=(prefecture_name)
+   self.prefecture_code = JpPrefecture::Prefecture.find(name: prefecture_name).code
+  end
+
   def User.search(search, user_or_book, how_search)
    if user_or_book == "1"
     if how_search == "1"
